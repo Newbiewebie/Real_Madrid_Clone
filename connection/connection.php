a11c058facaeeb4dd914acd591a9909a52e3932a
Newbiewebie <aswanipiyush2004@gmail.com>
2024-06-30 15:49:54 +0530

    Add files via upload

---
+++ connection/connection.php
@@ -0,0 +1,13 @@
+<?php
+error_reporting(0);
+session_start();
+$conn = mysqli_connect('localhost', 'root', '', 'piyush_madrid') or die('db not connected');
+
+// if ($conn) {
+//     echo "db connected";
+// } else {
+//     echo "error";
+// }
+
+
+?>
